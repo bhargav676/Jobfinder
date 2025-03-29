4e9fa94a30135b193b77f41334628aca7dcb38b6
Bhargav <bhargavk1290@gmail.com>
2025-03-29 16:14:25 +0530

    server last

--- server/server.js
+++ server/server.js
@@ -16,5 +16,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/jobs', jobRoutes);
 
+app.get('/',(req,res)=>{
+  res.send("job finder server started")
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
